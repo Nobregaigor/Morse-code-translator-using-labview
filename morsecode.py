0a04c1e74241b4836d214b0162d681c56f7b32ca
Azael del Rosario <azaeldrm@gmail.com>
2018-11-04 08:57:23 -0500

    Created config.py, motorcontroller.py and modified existing VIs.

--- morsecode.py
+++ morsecode.py
@@ -1,104 +1,23 @@
-import RPi.GPIO as GPIO
+import time
+import config
 
 def encodeMorseCode(string):
-
-    letters = {
-         'A':   '._',
-         'B':   '_...',
-         'C':   '_._.',
-         'D':   '_..',
-         'E':   '.',
-         'F':   '.._.',
-         'G':	'__.',
-         'H':	'...',
-         'I':	'..',
-         'J':	'.___',
-         'K':	'_._',
-         'L':	'._..',
-         'M':	'__',
-         'N':	'_.',
-         'O':	'___',
-         'P':	'.__.',
-         'Q':	'__._',
-         'R':	'._.',
-         'S':	'...',
-         'T':	'_',
-         'U':	'.._',
-         'V':	'..._',
-         'W':	'.__',
-         'X':	'_.._',
-         'Y':	'_.__',
-         'Z':	'__..',
-         '1':	'.____',
-         '2':	'..___',
-         '3':	'...__',
-         '4':	'...._',
-         '5':	'.....',
-         '6':	'_....',
-         '7':	'__...',
-         '8':	'___..',
-         '9':	'____.',
-         '0':	'_____'
-     }
-
-
-    phrase = []
+    letters = config.letters
+    phrase = ''
+    space = ','
     for c in string:
         morse = letters[c.upper()]
-        print(morse)
-        phrase.append(morse)
-        print(phrase)
+        phrase += morse + space
+    print(phrase)
 
 
-
-def decoderMorseCode(string):
+def decoderMorseCode(letters):
 # Function translate LabVIEW
-
-    letters = {
-        '._':   'A',
-        '_...': 'B',
-        '_._.': 'C',
-        '_..':  'D',
-        '.':    'E',
-        '.._.': 'F',
-        '__.':  'G',
-        '...':  'H',
-        '..':   'I',
-        '.___': 'J',
-        '_._':  'K',
-        '._..': 'L',
-        '__':   'M',
-        '_.':   'N',
-        '___':  'O',
-        '.__.': 'P',
-        '__._': 'Q',
-        '._.':  'R',
-        '...':  'S',
-        '_':    'T',
-        '.._':  'U',
-        '..._': 'V',
-        '.__':  'W',
-        '_.._': 'X',
-        '_.__': 'Y',
-        '__..': 'Z',
-        '.____':'1',
-        '..___':'2',
-        '...__':'3',
-        '...._':'4',
-        '.....':'5',
-        '_....':'6',
-        '__...':'7',
-        '___..':'8',
-        '____.':'9',
-        '_____':'0'
-    }
-
-    if string in letters:
-        return letters[string]
+    morseCode = config.morseCode
+    if string in morseCode:
+        return morseCode[letters]
     else:
         return ''
 
-def morseToMotor():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(22,GPIO.OUT)
-    v1 = GPIO.PWM(22,500)
+
+encodeMorseCode('SOS S')
---
+++ motorcontroller.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import config
+import morsecode
+
+
+def runMotor(dCycle): # Function that vibrates the motor.
+    motor = GPIO.PWM(21,600)
+    motor.start(0)
+    motor.ChangeDutyCycle(dCycle)
+    time.sleep(float(config.dt))
+    motor.stop()
+
+
+def main(): # Function that sets motor and pin, and vibrates to a phrase.
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(21,GPIO.OUT)
+    vibrateMorseCode('S')
+
+
+def vibrateMorseCode(phrase): # Function vibrates motors to a given phrase.
+    for c in phrase: # c is every character in a phrase.
+        if c == '.':
+            runMotor(100)
+            time.sleep(config.dt)
+        elif c == '_':
+            runMotor(70)
+            time.sleep(config.dt)
+        elif c == ',':
+            time.sleep(config.dt*2)
+        elif c == '%':
+            time.sleep(config.dt*6)
+        else:
+            pass
+
+
+main()
+GPIO.cleanup()
